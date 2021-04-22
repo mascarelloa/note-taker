2f4b73141f62f74aa4591d14f6db570b29ea748f
mascarelloa <amfm0793@gmail.com>
2021-04-22 00:13:58 -0400

    updating file structure and adding logic.

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+require(".routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+
+
+
+
+app.listen(PORT, () => console.log (`Listening on PORT: http://localhost:${PORT}`));
